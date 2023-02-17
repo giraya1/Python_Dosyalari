007010d5f9d378b42de7918956e659d52c399d35
Giray Aktaş <102739275+giraya1@users.noreply.github.com>
2023-02-17 13:46:51 +0300

    Phyton Liste

--- Listeproje/List.py
+++ Listeproje/List.py
@@ -1,26 +1,25 @@
-
-liste = {}
+liste = dict()
 a = 0
 
 while a < 3:
     a += 1
-    id = int(input("No giriniz:"))
+    no = int(input("No giriniz:"))
     ad = str(input("Ad giriniz:"))
     soyad = str(input("Soyad giriniz:"))
     yas = int(input("yaş giriniz:"))
 
     liste.update({
-        id: {
+        no: {
             "Adınız": ad,
             "Soyadınız": soyad,
             "Yaşınız": yas
         }
-
-    }
-
-    )
+    })
     print(liste)
 
-ovno = input("Aradığınız numarayı giriniz:")
-kisiler=liste[id]
-print(kisiler)
+ovno = int(input("Aradığınız numarayı giriniz:"))
+if ovno in liste:
+    kisiler = liste[ovno]
+    print(kisiler)
+else:
+    print("Kayıt bulunamadı")
